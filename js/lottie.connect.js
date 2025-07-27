db925984e9c270f0593578c7c77b343174ee274e
toxonpf <toxonpf@gmail.com>
2025-07-27 14:59:57 +0500

    Update lottie.connect.js

--- js/lottie.connect.js
+++ js/lottie.connect.js
@@ -3,5 +3,5 @@ var animation = lottie.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../img/json/buttonArrow.json'
+    path: 'https://raw.githubusercontent.com/toxonpf/portfolio/refs/heads/main/js/lottie.connect.js'
 });
